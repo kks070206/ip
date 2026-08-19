3f86dd6283d7b2e3ce32bf0392977cf616be2ced
Khor Kai Shean <khorkaishean@gmail.com>
2026-08-19 18:27:29 +0800

    Add greeting and exiting messages

--- src/main/java/Jason.java
+++ src/main/java/Jason.java
@@ -1,10 +1,10 @@
 public class Jason {
     public static void main(String[] args) {
-        String banner = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(banner);
+        String greetMessage = "Hello! My name is Jason, inspired by JSON files used by software engineers.";
+        String helpMessage = "How may I help you today?";
+        String endMessage = "Goodbye! Hope to see you again.";
+        System.out.println(greetMessage);
+        System.out.println(helpMessage);
+        System.out.println(endMessage);
     }
 }
