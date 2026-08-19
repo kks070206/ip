0db40e7f47ddf41159ee46b83e4a2ac9d7f544bc
Khor Kai Shean <khorkaishean@gmail.com>
2026-08-19 18:38:24 +0800

    Add echo feature

--- src/main/java/Jason.java
+++ src/main/java/Jason.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Jason {
     public static void main(String[] args) {
         String greetMessage = "Hello! My name is Jason, inspired by JSON files used by software engineers.";
@@ -5,6 +7,12 @@ public class Jason {
         String endMessage = "Goodbye! Hope to see you again.";
         System.out.println(greetMessage);
         System.out.println(helpMessage);
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(input);
+            input = sc.nextLine();
+        }
         System.out.println(endMessage);
     }
 }
